110d75accd47bfdf606bc5a630b819b23c6b14f8
Itay Saban <itaysaban@ngsoft.com>
2025-04-08 16:06:26 +0300

    changed mongo_uri

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,28 +2,31 @@ import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import { useAuthRoutes } from "./routes/auth";
 
 dotenv.config();
-
-const app = express();
-app.use(express.json());
-app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI as string;
 
-// Connect to MongoDB
-mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as mongoose.ConnectOptions)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// Setup routes
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+useAuthRoutes(app);
 
 // Basic route
 app.get("/", (req: Request, res: Response) => {
   res.send("Task Manager API is running...");
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Connect to MongoDB
+mongoose
+  .connect(MONGO_URI, {} as mongoose.ConnectOptions)
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
